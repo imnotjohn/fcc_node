72a2ab7fe40bc21c615521fc2cda43279f2162d6
didjrc <didjrc@nus.edu.sg>
2017-09-04 23:46:33 -0700

    learnyounode - HTTP Uppercaser Challenge - Solved

--- http-uppercaser.js
+++ http-uppercaser.js
@@ -9,18 +9,34 @@
 
 var fs = require('fs');
 var http = require('http');
-http.post = require('http-post');
-
 var port = process.argv[2];
 
 // npm package that you can use to 
 // tranform stream data as it's passed through.
 var map = require('through2-map');
 
-// test
 var server = http.createServer(function(request, response) {
-
+    request.pipe(map(function(chunk) {
+        return chunk.toString().toUpperCase();
+    })).pipe(response);
 });
-inStream.pipe(map(function (chunk) {
-    return chunk.toString().split('').reverse().join('');
-})).pipe(outStream);
+
+server.listen(port);
+
+// Solution:
+/*
+    var http = require('http')
+    var map = require('through2-map')
+
+    var server = http.createServer(function (req, res) {
+      if (req.method !== 'POST') {
+        return res.end('send me a POST\n')
+      }
+
+      req.pipe(map(function (chunk) {
+        return chunk.toString().toUpperCase()
+      })).pipe(res)
+    })
+
+    server.listen(Number(process.argv[2]))
+*/
